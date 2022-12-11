f70c655d609597c6548351136ba463a9b6ac752a
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-12-11 09:18:25 +0900

    シーンごとにポインタを用意しIDを割り当て

--- appOne/GAME.cpp
+++ appOne/GAME.cpp
@@ -1,7 +1,16 @@
 #include"libOne.h"
+#include "TITLE.h"
+#include "STAGE.h"
+#include "GAME_CLEAR.h"
+#include "GAME_OVER.h"
 #include "GAME.h"
 GAME::GAME() {
-
+    /// newとは？　→　メモリの動的管理？
+    Scenes[TITLE_ID] = new TITLE(this);
+    Scenes[STAGE_ID] = new STAGE(this);
+    Scenes[GAME_CLEAR_ID] = new GAME_CLEAR(this);
+    Scenes[GAME_OVER_ID] = new GAME_OVER(this);
+    CurSceneId = TITLE_ID;
 }
 GAME::~GAME() {
 
@@ -9,5 +18,6 @@ GAME::~GAME() {
 void GAME::run() {
     window(1920, 1080, full);
     while (notQuit) {
+        Scenes[CurSceneId]->proc();
     }
 }
--- appOne/GAME.h
+++ appOne/GAME.h
@@ -2,6 +2,17 @@
 class GAME
 {
 public:
+	enum SCENE_ID {
+		TITLE_ID,
+		STAGE_ID,
+		GAME_CLEAR_ID,
+		GAME_OVER_ID,
+		NUM_SCENES
+	};
+private:
+	class SCENE* Scenes[NUM_SCENES];
+	SCENE_ID CurSceneId;
+public:
 	GAME();
 	~GAME();
 	void run();
