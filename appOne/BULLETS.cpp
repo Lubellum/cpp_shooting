6cee033894845078514ebd1b89d8db60d9bf6efa
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-01-17 20:52:59 +0900

    BULLETクラスメンバの定義

--- appOne/BULLETS.cpp
+++ appOne/BULLETS.cpp
@@ -1,1 +1,13 @@
 #include "BULLETS.h"
+
+// 「：」でGAME_OBJECTクラスの継承を行っている？　→　#include "○○.h"とは別物？
+// そもそも#includeは「継承」と言うのか？
+BULLETS::BULLETS(class GAME* game):
+GAME_OBJECT(game){
+
+}
+BULLETS::~BULLETS(){}
+void BULLETS::create(){}
+void BULLETS::init(){}
+void BULLETS::update(){}
+void BULLETS::draw(){}
--- appOne/BULLETS.h
+++ appOne/BULLETS.h
@@ -7,10 +7,18 @@ class BULLETS :
 public:
 	struct DATA {
 		int totalNum;
+		// 現在の弾の数を定義し、無駄な出力を無くすアルゴリズムを組むそう
 		int curNum;
+		int img;
+		float advSpeed;
+		float angSpeed;
+		// bc = boundary circle(円を囲む) → キャラクタを囲む円の半径。当たり判定用
+		float bcRadius;
 	};
 private:
+	// 共通データ //
 	DATA Bullet;
+	// 単体データ配列 //
 	struct BULLET {
 		VECTOR2 pos;
 		VECTOR2 vec;
